5276174bc16d777fa4fa748043345dbfc7f3d90d
jbordeau <joan.bordeau@enovea.net>
2014-10-10 09:15:50 +0200

    fix a bug on .erlang_cookie

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'rabbitmq-server' ]; then
-	chown -R rabbitmq:rabbitmq /data
-fi
-
 /set_rabbitmq_password.sh
 /set_rabbitmq_erlang_cookie.sh
+
+if [ "$1" = 'rabbitmq-server' ]; then
+        chown -R rabbitmq:rabbitmq /data
+	chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
+fi
 
 exec "$@"
--- set_rabbitmq_erlang_cookie.sh
+++ set_rabbitmq_erlang_cookie.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [[ -z ${RABBITMQ_COOKIES} ]]; then
-	echo "update .erlang.cookie with this value : ${RABBITMQ_COOKIES}"
-	echo "${RABBITMQ_COOKIES}" > /var/lib/rabbitmq/.erlang.cookie
+        echo "RABBITMQ_COOKIES is unset"
+else	
+        echo "RABBITMQ_COOKIES is set : ${RABBITMQ_COOKIES}"
+        echo "${RABBITMQ_COOKIES}" > /var/lib/rabbitmq/.erlang.cookie
+	chmod 400 /var/lib/rabbitmq/.erlang.cookie
 fi
